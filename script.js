9e5038c83c86ba79544f649de7a432a3215dcf6f
Omega <elbackenddev22@gmail.com>
2024-12-03 11:06:42 -0600

    Ajustanto perfiles de profesores

--- script.js
+++ script.js
@@ -15,3 +15,17 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    let visitas = sessionStorage.getItem('contador_de_visitas');
+
+    if (!visitas) {
+        visitas = 0;
+    }
+
+    visitas = parseInt(visitas) + 1;
+
+    sessionStorage.setItem('contador_de_visitas', visitas);
+
+    document.querySelector('.contador_de_visitas').innerText = `Este sitio ha sido visitado ${visitas} veces en esta sesión.`;
+});
